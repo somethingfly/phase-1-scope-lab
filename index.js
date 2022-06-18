ce818cd82c03ba7841b515e2fedb7e30c3a8f3c1
Alfred <somethingfly@somethingfly.com>
2022-06-17 19:24:32 -0700

    commit tree ce7d7549dc40ecfe9612694b040bf941de2fdcc1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return
+}    
+
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+    return
+}
